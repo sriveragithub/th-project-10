ad81685a3c3b7f51a396df1dcfbb146351f94915
Skyler Rivera <skylerrivera17@yahoo.com>
2021-09-20 11:22:54 -0400

    created a private route component to ensure only authenticated users can access the page to create courses or edit courses

--- client/src/App.js
+++ client/src/App.js
@@ -15,6 +15,7 @@ import UserSignUp from './components/UserSignUp'
 import UserSignOut from './components/UserSignOut'
 
 import withContext from './Context'
+import PrivateRoute from './PrivateRoute'
 
 const UserSignInWithContext = withContext(UserSignIn)
 const UserSignUpWithContext = withContext(UserSignUp)
@@ -28,9 +29,9 @@ function App() {
         <HeaderWithContext />
         <Switch>
           <Route exact path="/" component={Courses} />
-          <Route exact path="/courses/create" component={CreateCourse} />
+          <PrivateRoute exact path="/courses/create" component={CreateCourse} />
           <Route exact path="/courses/:id" component={CourseDetail} />
-          <Route path="/courses/:id/update" component={UpdateCourse} />
+          <PrivateRoute path="/courses/:id/update" component={UpdateCourse} />
           <Route path="/signin" component={UserSignInWithContext} />
           <Route path="/signup" component={UserSignUpWithContext} />
           <Route path="/signout" component={UserSignOutWithContext} />
